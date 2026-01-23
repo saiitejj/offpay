852b6ecf2bccb86f715cca743bcfee38f285909a
Sai Tej <ngvst2004@gmail.com>
2026-01-24 01:14:08 +0530

    p->project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage(){
       <div className="container flex flex-col items-center justify-center gap-12 border px-4 py-16">
         
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          P- <span className="text-yellow-400"> #OP</span>
+          Project- <span className="text-yellow-400"> #OP</span>
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20">
