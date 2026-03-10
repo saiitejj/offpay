1caf6a552e75f81bbc1c304d90b0607eb4c00908
Sai Tej <ngvst2004@gmail.com>
2026-03-10 22:11:30 +0530

    found typo

--- src/app/(auth)/signup/actions.ts
+++ src/app/(auth)/signup/actions.ts
@@ -1,94 +1,98 @@
-"use server"
+"use server";
 
-import { db } from "../../../lib/db"
-import * as bcrypt from "bcryptjs"
-import { cookies } from "next/headers"
-import { isRedirectError } from "next/dist/client/components/redirect-error"
-import { createSession,deleteSession } from "~/lib/session"
-import { redirect } from "next/navigation"
+import { db } from "../../../lib/db";
+import * as bcrypt from "bcryptjs";
+import { cookies } from "next/headers";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { createSession, deleteSession } from "~/lib/session";
+import { redirect } from "next/navigation";
 
 interface RegisterCompanyData {
-    name?:string;
-    password?: string;
-    confirmPassword?: string;
-    companyName?: string;
-    employeeCount?: string;
-    employerName?: string;
-    email?: string;
+  name?: string;
+  password?: string;
+  confirmPassword?: string;
+  companyName?: string;
+  employeeCount?: string;
+  employerName?: string;
+  email?: string;
 }
 
-export async function logoutUser(){
-    await deleteSession()
-    redirect("/login")
+export async function logoutUser() {
+  await deleteSession();
+  redirect("/login");
 }
 
-export async function registerCompany(formData:RegisterCompanyData) {
-    try{ 
-
-        if (!formData.password || !formData.confirmPassword || !formData.email) {
-            return { error: "Missing required fields" }
-        }
-
-        if (formData.password !== formData.confirmPassword) {
-            return { error: "Passwords do not match" }
-        }
-        const passwordToHash =String(formData.password)
-        const hashedPassword :string =await bcrypt.hash(passwordToHash,10)
-        await db.company.create({
-            data:{
-                name:formData.companyName ?? "Unnamed Company",
-                employeeCount:Number.parseInt(formData.employeeCount ?? "0")||0,
-                users:{
-                    create:{
-                        name:formData.employerName ?? "Employer",
-                        email:formData.email,
-                        password:hashedPassword ,
-                        role:"MANAGER",
-                    }
-                }
-            }
-        })
-        return {success:true}
-    }catch(error:unknown){
-        console.error("SIGNUP_ERROR",error)
-        const errorMessage = error instanceof Error ? error.message : String(error);
-        
-        return {error:"Something went wrong or email already exists.",errorMessage}
+export async function registerCompany(formData: RegisterCompanyData) {
+  try {
+    if (!formData.password || !formData.confirmPassword || !formData.email) {
+      return { error: "Missing required fields" };
     }
 
-}
+    if (formData.password !== formData.confirmPassword) {
+      return { error: "Passwords do not match" };
+    }
+    const passwordToHash = String(formData.password);
+    const hashedPassword: string = await bcrypt.hash(passwordToHash, 10);
+    await db.company.create({
+      data: {
+        name: formData.companyName ?? "Unnamed Company",
+        employeeCount: Number.parseInt(formData.employeeCount ?? "0") || 0,
+        users: {
+          create: {
+            name: formData.employerName ?? "Employer",
+            email: formData.email,
+            password: hashedPassword,
+            role: "MANAGER",
+          },
+        },
+      },
+    });
+    return { success: true };
+  } catch (error: unknown) {
+    console.error("SIGNUP_ERROR", error);
+    const errorMessage = error instanceof Error ? error.message : String(error);
 
-export async function loginUser(formData:{email?:string;password?:string}) {
-    try{
-
-        if (!formData.email || !formData.password){
-            return {error: "Please enter your email and password"}
-        }
-    
-        const user=await db.user.findUnique({
-            where:{ email:formData.email}
-        })
-        if(!user){
-            return {error:"No user fount with this email"}
-        }
-    
-        const isPasswordCorrect=await bcrypt.compare(formData.password,user.password)
-    
-        if(!isPasswordCorrect){
-            return {error:"Incorrect password"}
-        }
-        await createSession(user.id,user.role)
-        redirect("/dashboard");
-    } catch (error) {
-    if (isRedirectError(error)) throw error;
-    
-    console.error("DEBUG_LOGIN_ERROR:", error); 
-    
-    return { 
-        error: error instanceof Error ? error.message : "An unknown error occurred" 
+    return {
+      error: "Something went wrong or email already exists.",
+      errorMessage,
     };
+  }
 }
 
-    
-    
+export async function loginUser(formData: {
+  email?: string;
+  password?: string;
+}) {
+  try {
+    if (!formData.email || !formData.password) {
+      return { error: "Please enter your email and password" };
+    }
+
+    const user = await db.user.findUnique({
+      where: { email: formData.email },
+    });
+    if (!user) {
+      return { error: "No user found with this email" };
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      formData.password,
+      user.password,
+    );
+
+    if (!isPasswordCorrect) {
+      return { error: "Incorrect password" };
+    }
+    await createSession(user.id, user.role);
+    redirect("/dashboard");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+
+    console.error("DEBUG_LOGIN_ERROR:", error);
+
+    return {
+      error:
+        error instanceof Error ? error.message : "An unknown error occurred",
+    };
+  }
 }
